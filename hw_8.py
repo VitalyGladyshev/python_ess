ee4d4533be4d2ca16d5df0e4cbc71405a2fe5674
VitalyGladyshev <VitalyGladyshev>
2021-07-19 22:29:37 +0300

    ДЗ 8 задача 1

--- hw_8.py
+++ hw_8.py
@@ -3,8 +3,6 @@
 ДЗ 8 Гладышев ВВ
 """
 
-# import datetime as dt
-
 """
 1. Реализовать класс «Дата», функция-конструктор которого должна принимать дату в виде строки формата «день-месяц-год».
 В рамках класса реализовать два метода. Первый, с декоратором @classmethod, должен извлекать число, месяц, год и
@@ -23,28 +21,90 @@ class Date:
 
     @classmethod
     def parse_date(cls, inc_date: str) -> tuple:
+        """
+        Выделение года, месяца и дня из строки
+        :param inc_date: str: строка с датой
+        :return: tuple:  year, month, day
+        """
         year: int = -1   # год
         month: int = -1  # месяц
         day: int = -1    # день
 
-        separators_list: list = [' ', '.', '/', '-']
-        split_list: list = []
+        separators_list: list = [' ', '.', '/', '-', '_', '\\']
+        split_list: list = [inc_date]
 
         for sp in separators_list:
-            if inc_date.count(sp) >= 2:
-                split_list = inc_date.split(sp)
+            internal_list: list = []
+            for el in split_list:
+                internal_list.extend(el.split(sp))
+            split_list = internal_list
 
-        return split_list  # year, month, day
+        numbers_list: list = []
+        for el in split_list:
+            if str(el).isdecimal() and len(el) < 5:
+                numbers_list.append(int(el))
+
+        len_four: int = 0
+        for ind in reversed(range(len(numbers_list))):
+            if len(str(numbers_list[ind])) == 4:
+                len_four = ind
+
+        if len(numbers_list) > 2:
+            if len_four:
+                day = numbers_list[0]
+                month = numbers_list[1]
+                year = numbers_list[2]
+            else:
+                year = numbers_list[0]
+                month = numbers_list[1]
+                day = numbers_list[2]
+
+        return year, month, day
 
     @staticmethod
-    def check_date():
-        pass
+    def check_date(check_str: str) -> bool:
+        date_corr: bool = True
+        year, month, day = Date.parse_date(check_str)
+        if not 0 <= year <= 3000:
+            date_corr = False
+        if not 1 <= month <= 12:
+            date_corr = False
+        if month in [1, 3, 5, 7, 8, 10, 12]:
+            if not 1 <= day <= 31:
+                date_corr = False
+        elif month in [4, 6, 9, 11]:
+            if not 1 <= day <= 30:
+                date_corr = False
+        elif month == 2:
+            if not 1 <= day <= 29:
+                date_corr = False
+        return date_corr
 
 
 answer = input("Задание 1 (д/н)? ")
 if answer == 'д':
     my_date = Date()
-    print(my_date.parse_date("1234.15.16 бла бла"))
+    date_1 = " бла бла _2020/12\\3"
+    print(f"\t{my_date.parse_date(date_1)}")
+    if my_date.check_date(date_1):
+        print("\t\tДата корректна")
+    else:
+        print("\t\tДата некорректна")
+
+    date_2 = "чих пых _1221_06_15"
+    print(f"\t{my_date.parse_date(date_2)}")
+    if my_date.check_date(date_2):
+        print("\t\tДата корректна")
+    else:
+        print("\t\tДата некорректна")
+
+    date_3 = "тыр 30/2/1689 пыр"
+    print(f"\t{my_date.parse_date(date_3)}")
+    if my_date.check_date(date_3):
+        print("\t\tДата корректна")
+    else:
+        print("\t\tДата некорректна")
+    print()
 
 """
 2. Создайте собственный класс-исключение, обрабатывающий ситуацию деления на нуль. Проверьте его работу на данных, 
